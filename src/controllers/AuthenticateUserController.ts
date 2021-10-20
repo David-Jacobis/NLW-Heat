8fa8cc728bf8d8e42831f5bdd46f587ee2f448fa
David Jacobis <d.jacobis@reply.com>
2021-10-20 12:12:24 -0300

    Erros corrigidos

--- src/controllers/AuthenticateUserController.ts
+++ src/controllers/AuthenticateUserController.ts
@@ -9,9 +9,12 @@ import { AuthenticateUserService } from "../services/AuthenticateUserService";
             const { code } = request.body;
 
             const service = new AuthenticateUserService();
+            try{
             const result = await service.execute(code);
-
             return response.json(result);
+            }catch(err){
+                return response.json({error: err.message});
+            }
         }
     }
 
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,18 +1,24 @@
 import axios from "axios";
-/**
- * Receber code(string)
- * Recuperar o acess_token no github
- * Verificar se o usuario existe no DB
- * ---SIM = Gera um token
- * ---Não = Cria no DB, gera um token
- * Retornar o token com as infos do user
- */
+
+
+
+
+interface IAccessTokenResponse{
+    access_token: string
+}
+
+interface IUserResponse{
+    avatar_url: string,
+    login: string,
+    id: number,
+    name: string 
+}
 
 class AuthenticateUserService {
 async execute(code: string){
-    const url = "https://github.com/login/oauth/acess_token";
+    const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post(url,null,{
+    const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url,null,{
         params:{
             client_id: process.env.GITHUB_CLIENT_ID,
             client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -21,11 +27,17 @@ async execute(code: string){
         headers: {
             "Accept": "application/json"
         }
+    });
+
+    const response = await axios.get<IUserResponse>("https://api.github.com/user",{
+        headers:{
+            authorization: `Bearer ${accessTokenResponse.access_token}`
+        }
     })
 
+    
+
     return response.data;
-
-
     }
 }
 export { AuthenticateUserService }
